ebae8fb94ad671e5203f9ad47476c07f6eb61372
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-15 20:03:44 +0200

    updated paddings

--- src/components/homepage/team/Teammate.tsx
+++ src/components/homepage/team/Teammate.tsx
@@ -22,7 +22,7 @@ export default function Teammate({ teammate, className }: TeammateProps) {
         height={640}
         className={`absolute top-0 left-0 -z-10 w-full h-full object-cover ${className}`}
       />
-      <div className="tab:flex items-center justify-between p-2 xl:py-4 xl:px-[14px] mt-auto rounded-[6px] bg-white">
+      <div className="tab:flex items-center justify-between p-[7px] xl:py-4 xl:px-[14px] mt-auto rounded-[6px] bg-white">
         <div>
           <p className="mb-[6px] xl:mb-2 text-10med xl:text-14med uppercase text-black leading-none">
             {name}
