e7b7da905e84891b3616d04872540da9935c90d0
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-18 13:47:17 +0200

    updated services texts

--- src/components/homepage/services/ServicesList.tsx
+++ src/components/homepage/services/ServicesList.tsx
@@ -7,7 +7,7 @@ interface ServicesListProps {
 
 export default function ServicesList({ servicesList }: ServicesListProps) {
   return (
-    <AnimatedListItem className="flex flex-col items-center w-[90%] max-w-[340px] tabxl:max-w-[380px] xl:max-w-[430px] h-[321px] xl:h-[492px] p-7 xl:p-[44px] mx-auto tabxl:mx-0 bg-white rounded-[8px]">
+    <AnimatedListItem className="flex flex-col items-center w-[90%] max-w-[340px] tabxl:max-w-[380px] xl:max-w-[430px] min-h-[336px] xl:min-h-[492px] p-7 xl:p-[44px] mx-auto tabxl:mx-0 bg-white rounded-[8px]">
       <h2 className="max-w-[204px] xl:max-w-[267px] mb-4 xl:mb-6 font-prosto text-20reg xl:text-28reg leading-[122%] text-center uppercase">
         {servicesList.title}
       </h2>
